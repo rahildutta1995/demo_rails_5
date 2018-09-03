3f823bdef3ee9d3129e93c1926bd1065df5da690
rahildutta1995 <rahil.max41@gmail.com>
2018-09-03 15:26:32 +0530

    test_case_learning

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,22 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title" , "Home"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title" , "Help"
+  end
+  test "static_pages_about_url" do 
+  	get static_pages_about_url
+  	assert_response :success
+    assert_select "title" , "About"
   end
 
+  test "should get root" do
+    get static_pages_home_url
+    assert_response :success
+  end
 end
