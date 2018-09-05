96de8d2af3482ecae85523931df096a02abede23
rahildutta1995 <rahil.max41@gmail.com>
2018-09-05 15:06:34 +0530

    login_logout_done_rspec

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,10 +24,11 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    
     @user = User.new(user_params) 
     if @user.save
+      login @user
       redirect_to root_url
+      flash[:notice] = "Welcome to the sample App"
     else
       render 'users/new'
     end
--- test/integration/login_user_test.rb
+++ test/integration/login_user_test.rb
@@ -4,6 +4,10 @@ class LoginUserTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = users(:Rahil)    
+  end
+
   test "login invalid user" do
   	get login_path
   	assert_template 'sessions/new'
@@ -11,4 +15,32 @@ class LoginUserTest < ActionDispatch::IntegrationTest
   	assert_template 'sessions/new'
   	get root_path
   end 
+
+  test "valid user login" do 
+    get login_path
+    post login_path,params: {sessions: {email: @user.email ,password: 'password'}}
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert is_logged_in?
+  end 
+
+  test "login with valid user and valid logging out" do 
+    get login_path
+    post login_path,params: {sessions: {email: @user.email ,password: 'password'}}
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]",login_path,count: 0
+    assert_select "a[href=?]",logout_path
+    assert_select "a[href=?]", user_path(@user)
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_select "a[href=?]",login_path
+    assert_select "a[href=?]",logout_path,count: 0
+    assert_select "a[href=?]", user_path(@user),count: 0
+  end
 end
