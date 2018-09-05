5b8590951c5eaafc9a3cb554a196a57f997e8e57
rahildutta1995 <rahil.max41@gmail.com>
2018-09-05 15:33:28 +0530

    advanced login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,12 +24,18 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_params) 
-    if @user.save
-      login @user
-      redirect_to root_url
-      flash[:notice] = "Welcome to the sample App"
+    @user = User.find_by(email: params[:user][:email])
+    if @user.nil?
+      @user = User.new(user_params) 
+      if @user.save
+        login @user
+        redirect_to root_url
+        flash[:notice] = "Welcome to the sample app"
+      else
+        render 'users/new'
+      end
     else
+      flash[:alert] = "User already present!"
       render 'users/new'
     end
   end
@@ -39,8 +45,6 @@ class UsersController < ApplicationController
   def update
     if @user.update(user_params)
       redirect_to root_url
-    else
-      redirect_to :back
     end
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+(1...3).each do |i|
+	User.create(name: "test",email: "test-#{i}@test.com",password: "password")
+end
+p "==========done=========="
